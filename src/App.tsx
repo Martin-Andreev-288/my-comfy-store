c34a115dc0982d173dbacca1efc48e102701def1
Martin Andreev <martin_i_a@abv.bg>
2024-08-21 15:28:41 +0300

    Install shadcn and delete redundant file
    First use of shadcn in App.tsx with Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Button } from "@/components/ui/button";
+// lecture 127. Shadcn/ui Install -> first example how to use it with button
+// there was some differences with the instructor, so we adjusted them according to its appearance
 function App() {
-  return <h1 className="text-7xl font-bold">App</h1>;
+  return (
+    <div>
+      <h1 className="text-7xl font-bold">App</h1>
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => console.log("it worked!!!")}
+      >
+        Click me
+      </Button>
+    </div>
+  );
 }
 
 export default App;
