2b9014c6d6c8281d79da08b4e529b300043743e2
Martin Andreev <martin_i_a@abv.bg>
2024-08-21 16:46:38 +0300

    Set redux toolkit and make a try to verify whether its setting is done correct

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
-// lecture 127. Shadcn/ui Install -> first example how to use it with button
-// there was some differences with the instructor, so we adjusted them according to its appearance
+import { useAppSelector } from "./hooks";
+
 function App() {
+  const { name } = useAppSelector((state) => state.userState);
+  console.log(name);
+
   return (
     <div>
       <h1 className="text-7xl font-bold">App</h1>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import { store } from "./store";
+import { Provider } from "react-redux";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
