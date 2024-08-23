3a61988a54a4b1fad2b644b8908a1429679ba834
Martin Andreev <martin_i_a@abv.bg>
2024-08-23 17:07:48 +0300

    Setup the landing page and create the initial structure for 5 different components

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,12 @@
+import { FeaturedProducts, Hero } from "@/components";
+// setup landing page. Create 5 different components, for now just the
+// structure. See ## Landing Components in the Readme to the right
 function Landing() {
-  return <h1 className='text-4xl'>Landing Page</h1>;
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
 }
 export default Landing;
