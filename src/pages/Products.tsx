538a61bb7149c443d6887b268b80f6242d081fd7
Martin Andreev <martin_i_a@abv.bg>
2024-08-24 18:16:02 +0300

    Extract query parameters and use them for a first time

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,9 +4,16 @@ import { type LoaderFunction } from "react-router-dom";
 
 const url = "/products";
 
-export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
-  const response = await customFetch<ProductsResponse>(url);
-  console.log(response);
+export const loader: LoaderFunction = async ({
+  request,
+}): Promise<ProductsResponse> => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+
+  const response = await customFetch<ProductsResponse>(url, { params });
+  console.log(response.data);
+
   return { ...response.data };
 };
 
