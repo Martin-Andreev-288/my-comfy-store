98412db38695c7e5853843739833e864f5152d3e
Martin Andreev <martin_i_a@abv.bg>
2024-08-24 14:18:47 +0300

    Setup of the products page with loader and create 3 additional (for now empty) components.
    The loader in app.tsx is just "cosmetically" replaced.
    In the next lectures we'll start to filter the data from this array with 10
    elements in it logged in the console from Products.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import {
 } from "./pages";
 import { ErrorElement } from "./components";
 import { loader as landingLoader } from "./pages/Landing";
+import { loader as productsLoader } from "./pages/Products";
 
 const router = createBrowserRouter([
   {
@@ -24,13 +25,14 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
-        loader: landingLoader,
         errorElement: <ErrorElement />,
+        loader: landingLoader,
       },
       {
         path: "products",
         element: <Products />,
         errorElement: <ErrorElement />,
+        loader: productsLoader,
       },
       {
         path: "products/:id",
