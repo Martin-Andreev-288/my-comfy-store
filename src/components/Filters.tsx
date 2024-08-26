9177c7a095dc82430c11870f3917e9dd93d8c81e
Martin Andreev <martin_i_a@abv.bg>
2024-08-26 15:17:32 +0300

    Implement form select
    Now we have select options for the categories, for the companies and for the order (it can be, for example, in alphabetical order).

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -2,6 +2,7 @@ import { Form, useLoaderData, Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { type ProductsResponseWithParams } from "@/utils";
 import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
 
 function Filters() {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams;
@@ -9,12 +10,33 @@ function Filters() {
 
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-      {/* search */}
+      {/* SEARCH */}
       <FormInput
         type="search"
         label="search product"
         name="search"
-        defaultValue="search"
+        defaultValue={search}
+      />
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        options={meta.categories}
+        defaultValue={category}
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label="select company"
+        name="company"
+        options={meta.companies}
+        defaultValue={company}
+      />
+      {/* ORDER */}
+      <FormSelect
+        label="order by"
+        name="order"
+        options={["a-z", "z-a", "high", "low"]}
+        defaultValue={order}
       />
       <Button type="submit" size="sm" className="self-end mb-2">
         search
