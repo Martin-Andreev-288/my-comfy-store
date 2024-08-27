3c188d048af72e3c524ef733642e357eeed2ea3a
Martin Andreev <martin_i_a@abv.bg>
2024-08-27 15:05:35 +0300

    Start to setup the fetch of single product with loader function
    Setup the type and the initial functionality.

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -11,11 +11,28 @@ import { Separator } from "@/components/ui/separator";
 import { SelectProductAmount, SelectProductColor } from "@/components";
 import { type LoaderFunction } from "react-router-dom";
 
-export const loader: LoaderFunction = async ({ params }) => {
-  return;
+export const loader: LoaderFunction = async ({
+  params,
+}): Promise<SingleProductResponse> => {
+  const response = await customFetch<SingleProductResponse>(
+    `/products/${params.id}`
+  );
+
+  return { ...response.data };
 };
 
-function SingleProduct() {
+const SingleProduct = () => {
+  const { data: product } = useLoaderData() as SingleProductResponse;
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const dollarsAmount = formatAsDollars(price);
+  const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const addToCart = () => {
+    console.log("add to cart");
+  };
+
   return <h1 className="text-4xl">SingleProduct Page</h1>;
-}
+};
 export default SingleProduct;
