aa7a2e249ff3bef0535715e123cba2e8edd430b4
Martin Andreev <martin_i_a@abv.bg>
2024-08-28 00:48:31 +0300

    Setup initial structure for the cart items list

--- src/components/CartItemColumns.tsx
+++ src/components/CartItemColumns.tsx
@@ -1,4 +1,19 @@
-function CartItemColumns() {
-  return <div>CartItemColumns</div>;
-}
-export default CartItemColumns;
+import { formatAsDollars } from "@/utils";
+import { useAppDispatch } from "@/hooks";
+import { Button } from "./ui/button";
+import { editItem, removeItem } from "@/features/cart/cartSlice";
+import SelectProductAmount from "./SelectProductAmount";
+import { Mode } from "./SelectProductAmount";
+
+export const FirstColumn = () => {
+  return <h4>First Column</h4>;
+};
+export const SecondColumn = () => {
+  return <h4>Second Column</h4>;
+};
+export const ThirdColumn = () => {
+  return <h4>Third Column</h4>;
+};
+export const FourthColumn = () => {
+  return <h4>Fourth Column</h4>;
+};
